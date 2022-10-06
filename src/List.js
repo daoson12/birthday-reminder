dcf96b0a0ed791ec7b1860c30c304ee97448efe0
daoson12 <daoduvictor48@gmail.com>
2022-10-06 21:31:24 +0000

    remodify list.app

--- src/List.js
+++ src/List.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
-const List = ({people}) => {
+const List = ({ people }) => {
+
+
+    const lotOfPeople = people.map((person) => {
+        const { id, name, age, image } = person;
+        return <article
+            key={id} className="person">
+            <img src={image} alt={name} />
+            <h4>{name}</h4>
+            <p>{age} years</p>
+        </article>
+    })
+
     return (
         <>
-          {people.map((person)=>{
-             const {id,name,age,image}=person;
-              return <article key={id} className="person">
-                  <img src={image} alt={name} />
-                  <h4>{name}</h4>
-                  <p>{age} years</p>
-              </article>
-          })}
+            {lotOfPeople}
         </>
     )
 }
